78b2c2cd26fc033a75223764ba2fbc6725e7cc96
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-10-11 12:05:11 +0000

    modificación de js, html y css

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {};
+window.onload = function() {
+  document.querySelector("#domain").innerHTML = domainGenerator();
+
+  function domainGenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let domainType = [".com", ".es"];
+
+    let domainNames = [];
+    for (let x = 0; x < pronoun.length; x++) {
+      for (let y = 0; y < adj.length; y++) {
+        for (let z = 0; z < noun.length; z++) {
+          for (let i = 0; i < domainType.length; i++) {
+            let domainName =
+              "<br>" + pronoun[x] + adj[y] + noun[z] + domainType[i] + "<br>";
+            domainNames.push(domainName);
+          }
+        }
+      }
+    }
+    console.log(domainNames);
+    return domainNames;
+  }
+};
